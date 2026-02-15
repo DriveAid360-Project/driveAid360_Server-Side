c95d8726d1118d6ec0ff0242fc60d170c00d7247
“damirumayadunne” <you@example.com>
2026-02-15 17:52:01 +0530

    Initial commit

---
+++ src/main/java/lk/project/deiveaid360/Deiveaid360Application.java
@@ -0,0 +1,13 @@
+package lk.project.deiveaid360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deiveaid360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deiveaid360Application.class, args);
+	}
+
+}
